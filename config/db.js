0246a4d392813453eb7a28701d6c16089a24204f
Abhishek6416 <abhishekkr6416@gmail.com>
2024-05-31 11:28:27 +0530

    done

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const DB_CONNECT = async () => {
   const URL = process.env.MONGO_URL;
   return mongoose
-    .connect(URL)
+    .connect(URL,{serverSelectionTimeoutMS: 30000, // 30 seconds
+    connectTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 45000 })
     .then((res) => console.log(res.connection.db.databaseName))
     .catch((er) => console.log(er));
 };
